5df446e183f126f707386eaf020718d5b03708be
Krishanu <deyk@tcd.ie>
2020-12-23 14:58:58 +0530

    mask_and_cases uses new_cases as a feature now.

--- cases_and_masks/linearRegression.py
+++ cases_and_masks/linearRegression.py
@@ -9,10 +9,10 @@ def main():
     df=pd.read_csv("mask_with_daily_cases.csv",comment='#')
 
     # Retrieve all data.
-    global state, dates, feature_1_mask, target_value_new_cases
+    global state, dates, feature_1_mask, new_cases
     state = df.iloc[:,0]
     dates = df.iloc[:,1]
-    target_value_new_cases = df.iloc[:,2]
+    new_cases = df.iloc[:,2]
     feature_1_mask = df.iloc[:,3]
 
     # Collect only first State data.
@@ -21,41 +21,56 @@ def main():
     while state[counter] == first_state:
         counter += 1
 
-    days_delay = 14
+    days_delay = 7
     feature_1_mask = feature_1_mask[0 : counter - days_delay]
-    target_value_new_cases = target_value_new_cases[days_delay : counter]
+    feature_1_mask = feature_1_mask/100
+
+    new_cases = new_cases[days_delay : counter]
+    new_cases = new_cases/new_cases.max()
+
     dates = dates[days_delay : counter]
 
-    X = np.column_stack((feature_1_mask, target_value_new_cases))
+    q = 7
+    dd = 1
+    lag = 3
+    #q−step ahead prediction
+    stride = 1
+    XX = new_cases[0:new_cases.size - q - lag * dd:stride]
+    for i in range(1,lag):
+        X = new_cases[i*dd:new_cases.size - q - (lag - i) * dd:stride]
+        M = feature_1_mask[i*dd:feature_1_mask.size - q - (lag - i) * dd:stride]
+        XX = np.column_stack((XX,X,M))
+    yy = new_cases[lag*dd+q::stride]
+    tt = dates[lag*dd+q::stride]
 
     from sklearn.linear_model import LinearRegression
-    model = LinearRegression().fit(X, target_value_new_cases)
+    model = LinearRegression().fit(XX, yy)
 
     fig = plt.figure()
 
-    predictions = model.predict(X)
+    predictions = model.predict(XX)
     ax = fig.add_subplot(3, 1, 1)
     ax.xaxis.set_major_locator(plt.MaxNLocator(10))
-    dots = ax.scatter(dates, target_value_new_cases, color='r')
-    ax.plot(dates, predictions)
+    dots = ax.scatter(dates, new_cases, color='r')
+    ax.plot(tt, predictions)
 
     fake2Dline = mpl.lines.Line2D([0],[0], linestyle="none", c='b', marker = 'o')
     ax.legend([fake2Dline, dots], ['Predictions', 'Training data'], numpoints = 1)
     ax.set_xlabel('Dates (MM/DD/YY)')
     ax.set_ylabel('Target Value - New Cases')
     ax.title.set_text('Daily New Cases in Alabama')
-
+    """
     ax = fig.add_subplot(3, 1, 3)
     ax.xaxis.set_major_locator(plt.MaxNLocator(10))
-    ax.plot(dates, X[:,0])
+    ax.scatter(tt, new_cases)
     ax.set_xlabel('Dates (MM/DD/YY)')
     ax.set_ylabel('Masks Usage Percent')
     ax.title.set_text('Mask Usage over Time in Alabama')
-
-    #plt.show()
+    """
+    plt.show()
 
     from sklearn.metrics import mean_squared_error
-    print(mean_squared_error(target_value_new_cases, predictions))
+    print(mean_squared_error(yy, predictions))
 
 
 if __name__ == "__main__":
--- cases_and_masks/models.py
+++ cases_and_masks/models.py
@@ -10,10 +10,10 @@ def main():
     df=pd.read_csv("mask_with_daily_cases.csv",comment='#')
 
     # Retrieve all data.
-    global state, dates, feature_1_mask, target_value_new_cases
+    global state, dates, feature_1_mask, new_cases
     state = df.iloc[:,0]
     all_dates = df.iloc[:,1]
-    all_target_value_new_cases = df.iloc[:,2]
+    all_new_cases = df.iloc[:,2]
     all_feature_1_mask = df.iloc[:,3]
 
     # Collect only first State data.
@@ -22,24 +22,33 @@ def main():
     while state[counter] == first_state:
         counter += 1
 
-    days_delays = [0, 7, 14, 21]
+    days_delays = [7, 14, 21, 28]
 
     for days_delay in days_delays:
       print("Mean Square Errors for each Model with delay offset being " + str(days_delay) + " days:")
 
-      feature_1_mask = all_feature_1_mask[0 : counter - days_delay]
-      target_value_new_cases = all_target_value_new_cases[days_delay : counter]
-      dates = all_dates[days_delay : counter]
+      feature_1_mask = all_feature_1_mask/100
+      new_cases = all_new_cases/all_new_cases.max()
+      dates = all_dates
 
-      X = np.column_stack((feature_1_mask, target_value_new_cases))
-      y = np.array(target_value_new_cases)
+      q = 7
+      dd = 1
+      lag = days_delay
+      #q−step ahead prediction
+      stride = 1
+      XX = new_cases[0:new_cases.size - q - lag * dd:stride]
+      for i in range(1,lag):
+        X = new_cases[i*dd:new_cases.size - q - (lag - i) * dd:stride]
+        M = feature_1_mask[i*dd:feature_1_mask.size - q - (lag - i) * dd:stride]
+        XX = np.column_stack((XX,X,M))
+      yy = new_cases[lag*dd+q::stride]
 
       # Linear Regression
       from sklearn.linear_model import LinearRegression
-      model = LinearRegression().fit(X, target_value_new_cases)
-      predictions = model.predict(X)
+      model = LinearRegression().fit(XX, yy)
+      predictions = model.predict(XX)
       from sklearn.metrics import mean_squared_error
-      print("Linear Regression: " + str(mean_squared_error(target_value_new_cases, predictions)))
+      print("Linear Regression: " + str(mean_squared_error(yy, predictions)))
 
       # Lasso with multiple alpha values
       mean_errors=[]
@@ -49,15 +58,11 @@ def main():
           clf = linear_model.Lasso(alpha=alphaI)
           temp=[]
           
-          from sklearn.model_selection import KFold
-          kf = KFold(n_splits=5)
+          clf.fit(XX, yy)
+          predictions = clf.predict(XX)
 
-          for train, test in kf.split(X):
-              clf.fit(X[train], y[train])
-              predictions = clf.predict(X[test])
-
-              from sklearn.metrics import mean_squared_error
-              temp.append(mean_squared_error(y[test], predictions))
+          from sklearn.metrics import mean_squared_error
+          temp.append(mean_squared_error(yy, predictions))
 
           mean_errors.append(np.array(temp).mean())
       print("Lasso: ")
@@ -72,40 +77,35 @@ def main():
           clf = linear_model.Ridge(alpha=alphaI)
           temp=[]
           
-          from sklearn.model_selection import KFold
-          kf = KFold(n_splits=5)
+          clf.fit(XX, yy)
+          predictions = clf.predict(XX)
 
-          for train, test in kf.split(X):
-              clf.fit(X[train], y[train])
-              predictions = clf.predict(X[test])
-
-              from sklearn.metrics import mean_squared_error
-              temp.append(mean_squared_error(y[test], predictions))
+          from sklearn.metrics import mean_squared_error
+          temp.append(mean_squared_error(yy, predictions))
 
           mean_errors.append(np.array(temp).mean())
-
       print("Ridge: ")
       for i in range(len(mean_errors)):
         print("\tWith alpha " + str(alphas[i]) + " MSE is: " + str(mean_errors[i]))
 
       # MLP Regressor
       from sklearn.neural_network import MLPRegressor
-      model = MLPRegressor(hidden_layer_sizes=(200), alpha=0.01).fit(X, target_value_new_cases)
-      predictions = model.predict(X)
-      print("MLP: " + str(mean_squared_error(target_value_new_cases, predictions)))
+      model = MLPRegressor(hidden_layer_sizes=(200), alpha=0.01).fit(XX, yy)
+      predictions = model.predict(XX)
+      print("MLP: " + str(mean_squared_error(yy, predictions)))
 
 
       print("Baseline Model: ")
       from sklearn.dummy import DummyRegressor
       dummy_clf = DummyRegressor(strategy="mean")
-      dummy_clf.fit(X, target_value_new_cases)
-      predictions = model.predict(X)
-      print("\tMean:" + str(mean_squared_error(target_value_new_cases, predictions)))
+      dummy_clf.fit(XX, yy)
+      predictions = dummy_clf.predict(XX)
+      print("\tMean:" + str(mean_squared_error(yy, predictions)))
 
       dummy_clf = DummyRegressor(strategy="median")
-      dummy_clf.fit(X, target_value_new_cases)
-      predictions = model.predict(X)
-      print("\tMedian:" + str(mean_squared_error(target_value_new_cases, predictions)))
+      dummy_clf.fit(XX, yy)
+      predictions = dummy_clf.predict(XX)
+      print("\tMedian:" + str(mean_squared_error(yy, predictions)))
 
       print()
     
